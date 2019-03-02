671205e643b077880ef1392a4747db535b5e3373
BhargavD <bbdeshpa@ncsu.edu>
2019-03-02 15:54:46 -0500

    local Storage added and signin embedded in popup.html

--- src/options.js
+++ src/options.js
@@ -3,10 +3,14 @@ let setGitToken = document.getElementById('setGitToken');
   function constructOptions() {
       setGitToken.onclick = function() {
         let token_value = document.getElementById("gitHubToken").value;
-        chrome.storage.sync.set({gitToken: token_value}, function() 
+		localStorage.gitToken = token_value;
+		document.getElementById("notification_list").style.display = "block";
+    	document.getElementById("mark_all_read").style.display = "block";
+    	 document.getElementById("loginDetails").style.display = "none";
+        /*chrome.storage.local.set({gitToken: token_value}, function() 
         {
-          console.log('token is ' + token_value);
-        })
+          console.log('token ' + token_value+ ' saved successfully');
+        })*/
     }
 }
 constructOptions();

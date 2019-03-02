cb860472cf43aab30ff512a0a9ff08d01c19c696
drupadhy <drupadhy@ncsu.edu>
2019-03-01 19:27:15 -0500

    Update options.js

--- src/options.js
+++ src/options.js
@@ -1,8 +1,10 @@
 let setGitToken = document.getElementById('setGitToken');
+
   function constructOptions() {
       setGitToken.onclick = function() {
         let token_value = document.getElementById("gitHubToken").value;
-        chrome.storage.sync.set({gitToken: token_value}, function() {
+        chrome.storage.sync.set({gitToken: token_value}, function() 
+        {
           console.log('token is ' + token_value);
         })
     }
